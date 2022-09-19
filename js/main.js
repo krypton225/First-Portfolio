d416ced7e00cb43960bfaecdb16772a08975c82c
krypton225 <k.mohamed.11.98@gmail.com>
2022-09-19 22:32:22 +0200

    ADD main & home sections

---
+++ js/main.js
@@ -0,0 +1,58 @@
+showPicLanding();
+showTextLandingPage();
+layersSystem();
+
+function showPicLanding() {
+    let picLanding = document.getElementById("picLanding");
+
+    setTimeout(() => {
+        picLanding.classList.add("show-pic");
+    }, 1300);
+}
+
+function showTextLandingPage() {
+    AOS.init({
+        disable: false,
+        startEvent: 'DOMContentLoaded',
+        initClassName: 'aos-init',
+        animatedClassName: 'aos-animate',
+        useClassNames: false,
+        disableMutationObserver: false,
+        debounceDelay: 50,
+        throttleDelay: 99,
+        offset: 120,
+        delay: 0,
+        duration: 400,
+        easing: 'ease-in-out',
+        once: false,
+        mirror: false,
+        anchorPlacement: 'top-bottom',
+    });
+}
+
+function layersSystem() {
+    let allLinks = Array.from(document.querySelectorAll(".main-bg .menu li a"));
+
+    // * Get all ID's sections
+    let homeSection = document.getElementById("home");
+    let secondSection = document.getElementById("second");
+
+    allLinks.forEach((myLink) => {
+        myLink.addEventListener("click", () => {
+
+            document.querySelectorAll("section").forEach((sec) => {
+                sec.classList.remove("high-index");
+
+                if (sec.getAttribute("id") !== "home") {
+                    homeSection.classList.add("add-props");
+                }
+            });
+
+            if (myLink.getAttribute("id") === "link-home") {
+                homeSection.classList.add("high-index");
+            } else if (myLink.getAttribute("id") === "link-about") {
+                secondSection.classList.add("high-index");
+            }
+        })
+    })
+}
