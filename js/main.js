06c023055889fed0cf6ecafea782fb9dba656db0
krypton225 <k.mohamed.11.98@gmail.com>
2022-09-23 17:20:11 +0200

    REM unneeded files

--- js/main.js
+++ js/main.js
@@ -21,7 +21,6 @@ window.addEventListener("load", () => {
 })
 
 showPicLanding();
-showTextLandingPage();
 layersSystem();
 swiperWorks();
 swiperTestimonials();
@@ -33,26 +32,6 @@ function showPicLanding() {
     setTimeout(() => {
         picLanding.classList.add("show-pic");
     }, 1300);
-}
-
-function showTextLandingPage() {
-    AOS.init({
-        disable: false,
-        startEvent: 'DOMContentLoaded',
-        initClassName: 'aos-init',
-        animatedClassName: 'aos-animate',
-        useClassNames: false,
-        disableMutationObserver: false,
-        debounceDelay: 50,
-        throttleDelay: 99,
-        offset: 120,
-        delay: 0,
-        duration: 400,
-        easing: 'ease-in-out',
-        once: false,
-        mirror: false,
-        anchorPlacement: 'top-bottom',
-    });
 }
 
 function layersSystem() {
