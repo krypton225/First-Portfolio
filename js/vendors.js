79790198721377c8a0244f71518fc6ccdf527367
krypton225 <k.mohamed.11.98@gmail.com>
2022-11-21 01:21:42 +0200

    REM fontawesome & ADD fontello icons

--- js/vendors.js
+++ js/vendors.js
@@ -1,5 +1,2 @@
 // * Import Bootstrap bundle js file.
 import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-
-// * Import Fontawesome js file.
-import "../../node_modules/@fortawesome/fontawesome-free/js/all.min.js";
