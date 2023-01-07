483331001eec5459fcd0340b97c75be4b71de2f3
krypton225 <k.mohamed.11.98@gmail.com>
2023-01-07 22:48:49 +0200

    ADD webpack & bundle js files

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./js/main.js",
+    output: {
+        filename: "main.bundle.js",
+        path: path.resolve(__dirname, "dist/js"),
+    },
+    mode: "production"
+};
