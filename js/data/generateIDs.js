695093ffcdfb5921df56f95865c2d5ab1d99121f
krypton225 <k.mohamed.11.98@gmail.com>
2023-02-11 18:46:12 +0200

    EDIT generateID module with new version

--- js/data/generateIDs.js
+++ js/data/generateIDs.js
@@ -1,18 +1,45 @@
 const generateID = (function () {
-    function* MainCountingIDGenerator(start = 1) {
-        let count = start;
+    /**
+      * @returns {number}
+    */
+    function* getNumberID() {
+        let count = 1;
 
         while (true) {
             yield count++;
         }
     }
 
-    function insertIDIntoData(dataArr = []) {
-        const counter = MainCountingIDGenerator();
+    /**
+      * @param  {Array} sourceArr=[] - array to check if it has objects or not.
+    */
+    function isArrayNotEmpty(sourceArr = []) {
+        return sourceArr.length !== 0
+    }
 
-        dataArr.forEach((card) => {
-            card.id = counter.next().value;
-        });
+    /**
+      * @param  {Array} dataArr=[] - array has objects and insert IDs into each one.
+    */
+    function insertIDIntoData(dataArr = []) {
+        if (isArrayNotEmpty(dataArr)) {
+            const counter = getNumberID();
+
+            dataArr.forEach((row) => {
+                if (typeof row === "object") {
+                    if (!row.hasOwnProperty("id")) {
+                        Object.defineProperty(row, "id", {
+                            value: counter.next().value,
+                            writable: false,
+                            enumerable: true
+                        });
+                    }
+                } else {
+                    throw new Error(`Array ${dataArr.name} must contain only object.`);
+                }
+            });
+        } else {
+            throw new Error("Array must not be empty of objects.");
+        }
     }
 
     return {
